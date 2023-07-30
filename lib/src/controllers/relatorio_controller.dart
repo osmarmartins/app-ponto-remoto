087449ecece5a3fc688c8d5c8ce785818585455f
osmar-dell <osmarfilh6@gmail.com>
2023-07-29 22:49:06 -0300

    (add) Carregamento dos pontos no banco de dados ao iniciar o app

--- lib/src/controllers/relatorio_controller.dart
+++ lib/src/controllers/relatorio_controller.dart
@@ -1,9 +1,18 @@
 import 'package:get/get.dart';
+import 'package:ponto_remoto/src/data/ponto_dao.dart';
+import 'package:ponto_remoto/src/helpers/database_helper.dart';
 
 import '../models/ponto.dart';
 
 class RelatorioController extends GetxController {
   RxList<Ponto> pontos = <Ponto>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    var pontoDao = PontoDAO();
+    pontoDao.findAll().then((value) => pontos.assignAll(value));
+  }
 
   set items(List<Ponto> items) {
     pontos.assignAll(items);
